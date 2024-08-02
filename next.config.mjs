eaa2e448b689ec12c4e0945e2e74babe5d5e1d81
Muhammad Suleman Ali Asif <suleman.ali.asif@outlook.com>
2024-08-02 12:07:33 +0500

    Update the next-.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-  }
-  
-  export default nextConfig;
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
+
+export default nextConfig;
